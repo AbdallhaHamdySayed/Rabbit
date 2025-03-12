fe3b133a906a01f490c71e500a4eaa502090d09a
Abdallha <126670745+AbdullahJava@users.noreply.github.com>
2025-03-12 22:07:10 +0200

    207. Develop message microservice using Spring Cloud Functions - Part 1

--- src/main/java/com/example/message/dto/AccountsMsgDto.java
+++ src/main/java/com/example/message/dto/AccountsMsgDto.java
@@ -1,4 +1,10 @@
 package com.example.message.dto;
 
-public record AccountsMsgDto() {
+/**
+ * @param accountNumber
+ * @param name
+ * @param email
+ * @param mobileNumber
+ */
+public record AccountsMsgDto( Long accountNumber, String name, String email, String mobileNumber) {
 }
